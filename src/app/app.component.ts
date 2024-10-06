426c0f7ae706397d15070e85b1d5b31bb7101673
Diego Oliveira <diegolvr14@gmail.com>
2024-10-05 21:57:26 -0300

    feat: adicionando a lista de compras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaComprasComponent } from "./lista-compras/lista-compras.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaComprasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'lista-compras', component: ListaComprasComponent },
+];
